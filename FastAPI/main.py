4aefebadbae8254ed771750ee0e8e787d775e3aa
DustinBaek <dustinbaek@deloitte.co.uk>
2023-02-13 09:19:23 +0000

    4 corner shops, error handling, datetime fixed

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,102 +1,100 @@
 """
 Main file for the template application
 """
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
+from enum import Enum
 import uvicorn
 from typing import Optional, Dict
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Initialise FastAPI App object
 app = FastAPI()
 
 class Item(BaseModel):
-    datetime: datetime 
-    geocode: Optional[str] = None
-    input_address:Optional[str] = None
-    lat:Optional[str] = None
-    lng:Optional[str] = None
+    store_name:str
     product_type: str 
-    resolved_address:Optional[str] = None
     stock_level: int 
 
 class Inventory(BaseModel):
-     items: Dict[int, Item]
+     items: Dict[str, Item]
+
+class StoreName(str, Enum):
+     Sainsbury = "Sainsbury"
+     Coop = "Coop"
+     Waitrose = "Waitrose"
+     Aldi = "Aldi"
 
 item1 = Item(
-    datetime=datetime.now(),
-    geocode= "223341463921",
-    input_address="WC1H 9NU",
-    lat="223341",
-    lng="463921",
+    store_name="Sainsbury",
     product_type="toilet_paper",
-    resolved_address="undefined",
     stock_level=3
     )
 
-item2 = Item(datetime=datetime.now(),
-    geocode= "223341463921",
-    input_address="WC1H 9NU",
-    lat="223341",
-    lng="463921",
+item2 = Item(
+    store_name="Coop",
     product_type="soap",
-    resolved_address="undefined",
     stock_level=5)
-inventory = Inventory(items = {1:item1, 2:item2})
+
+def add_datetime(inventory):
+     for item_id in inventory.items:
+          item_dict = inventory.items[item_id].dict()
+          item_dict["datetime"] = datetime.now() - timedelta(1)
+          inventory.items[item_id] = item_dict
+          
+inventory = Inventory(items = {"ZG011AQA":item1, "AZ246QST":item2})
+add_datetime(inventory)
+
 
 @app.get("/")
-async def root():
+async def all_stocklevels():
     return inventory.items
 
-@app.get("/get-item/{item_id}")
-async def get_stocklevel(item_id: int = Path(None, description="The ID of the item you would like to view", gt=0)):
+@app.get("/store_names/{store_name}")
+async def get_store_name(store_name: StoreName):
+     keys = [k for k,v in inventory.items.items() if v["store_name"]==store_name]
+     if keys:
+          return {"Store_name": store_name}
+     raise HTTPException(status_code=404, detail="Store not found")
+
+@app.get("/get-item/{item_id}") 
+async def get_stocklevel(item_id: str = Path(None, description="The ID of the item you would like to view")):
         if item_id not in inventory.items.keys():
-            return {
-                "Error": "Given ID does not exist"
-                }           
-        return { "Product Type is": inventory.items[item_id].product_type,
-                "Stock Level of" : inventory.items[item_id].stock_level
+          raise HTTPException(status_code=404, detail="Given ID does not exist")
+       
+        return { "Store Name is ": inventory.items[item_id]["store_name"],
+             "Product Type is": inventory.items[item_id]["product_type"],
+                "Stock Level of" : inventory.items[item_id]["stock_level"],
+                "Datetime": inventory.items[item_id]["datetime"]
                 }
 
-@app.get("/get-by-product-type")
-async def get_item(name: str = Query(None,  description="Type of item")):
-        keys = [k for k,v in inventory.items.items() if v.product_type==name]
-        
-        return [inventory.items.get(key) for key in keys]
-        # for item_id in inventory.items: ####for loop doesn't work?####
-            # if inventory.items[item_id].product_type == name:
-            #     return inventory.items[item_id]
-            # else: 
-            #     #  return {"Error":"Does not exist"}
-            #      return name
+@app.get("/get-by-store-and-product-name")
+async def get_item_by_store(store_name: StoreName, name: str = Query(None,  description="Type of item")):
+     keys = [k for k,v in inventory.items.items() if v["store_name"]==store_name and v["product_type"]==name]
+     if keys:
+          return [inventory.items.get(key) for key in keys]
+     raise HTTPException(status_code=404, detail=f"{store_name} does not have any stock of {name}")
+
         
 @app.post("/create-stocklevel/{item_id}")
-async def post_stocklevel(item_id: int, item: Item):
+async def post_stocklevel(item_id: str, item: Item):
+     item_dict = item.dict()
      if item_id in inventory.items.keys():
-          return {"Error": "Item ID already exists"}
-     inventory.items[item_id] = item
+          raise HTTPException(status_code=404, detail="Item ID already exists")
+     item_dict["datetime"] = datetime.now()
+     inventory.items[item_id] = item_dict
+
      return inventory.items[item_id]
 
 @app.put("/update-item/{item_id}")
-async def update_stocklelve(item_id: int, item: Item):
+async def update_stocklevel(item_id: str, item: Item):
      item_dict = item.dict()
      if item_id not in inventory.items.keys():
-          return {"Error": "Item ID does not exists"}
+          raise HTTPException(status_code=404, detail="Item ID does not exists")
+     item_dict["datetime"] = datetime.now()
      inventory.items[item_id]=item_dict
      return inventory.items[item_id]
 
 
 if __name__ == "__main__":
      uvicorn.run(app, host="0.0.0.0", port=8000)
-    # for item_id in inventory.items:
-    #     if inventory.items[item_id].product_type == 'soap':
-    #         print(inventory.items[item_id])
-    #  for item_id in inventory.items.keys():
-    #       if inventory.items[item_id].product_type == 'toilet_paper':
-    #         print(item_id)
-    #  if 1 in inventory.items.keys():
-    #     print('yep')
-    #     print(inventory.items[1].geocode)
-    #  else:
-    #     print(inventory.items.keys())
-    # # main()
