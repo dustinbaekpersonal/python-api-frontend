cf66c7da6127ab707ff049443b786923e4b93cf8
Dustin Baek <dustinbaek@deloitte.co.uk>
2024-02-29 20:11:45 +0000

    run test on root

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,14 +1,7 @@
 """Test for main.py."""
-# from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from api.main import app
-
-# app = FastAPI()
-
-# @app.get("/")
-# async def read_main():
-#     return {"msg": "Hello World"}
 
 test_client = TestClient(app)
 
@@ -17,4 +10,4 @@ def test_root():
     """Unit test for root get API call."""
     response = test_client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {"message": "Welcome to FastAPI Course!"}
