2d6ccf1a83f72a378807aeb60216035c4d5b07cb
Dustin Baek <dustinbaek@deloitte.co.uk>
2024-02-29 22:56:05 +0000

    add test using httpx.asyncclient

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,13 +1,45 @@
 """Test for main.py."""
+from typing import AsyncIterator
+
+import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
 from api.main import app
 
-test_client = TestClient(app)
+
+### Using TestClient, which inherits from httpx.Client###
+@pytest.fixture(scope="session")
+def client():
+    """Test Client."""
+    return TestClient(app)
 
 
-def test_root():
+def test_root(client: TestClient) -> None:
     """Unit test for root get API call."""
-    response = test_client.get("/")
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to FastAPI Course!"}
+
+
+### Using httpx.AsyncClient ###
+@pytest.fixture()
+async def async_client() -> AsyncIterator[AsyncClient]:
+    """Async client."""
+    async with AsyncClient(app=app, base_url="http://testserver") as client:
+        yield client
+
+
+# To run test only on asyncio backend, not trio backend
+@pytest.fixture()
+def anyio_backend() -> str:
+    """Letting anyio know that we run only on asyncio."""
+    return "asyncio"
+
+
+@pytest.mark.anyio
+async def test_root_async(async_client: AsyncClient) -> None:
+    """Unit test for root get api call using async."""
+    response = await async_client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to FastAPI Course!"}
