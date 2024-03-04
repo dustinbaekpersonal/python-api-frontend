2c93a8f9c000f335d608892e12dbbab6d668a1cb
Dustin Baek <dustinbaek@deloitte.co.uk>
2024-03-04 20:27:58 +0000

    fix linting

--- tests/unit/test_items.py
+++ tests/unit/test_items.py
@@ -21,21 +21,19 @@ client = TestClient(app)
                     "product_name": "milk",
                     "stock_level": 0,
                     "store_id": 1,
-                    "updated_date": "2024-03-03T21:41:00.529000"
+                    "updated_date": "2024-03-03T21:41:00.529000",
                 },
             ],
         ),  # successful API call
         (
             "Aldi",
             404,
-            {'detail': "Store 'Aldi' not found."},
+            {"detail": "Store 'Aldi' not found."},
         ),  # wrong API call
     ],
 )
 def test_get_stock_levels_by_store_name(
-    store_name: str,
-    expected_status: int,
-    expected_response: dict[str,Any]
+    store_name: str, expected_status: int, expected_response: dict[str, Any]
 ) -> None:
     """Unit test for get_stock_levels."""
     response = client.get(f"/inventory/{store_name}/")
