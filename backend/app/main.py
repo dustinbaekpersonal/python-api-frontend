4a69f949ef21faac1d22818cf813c3725a8714ed
Dustin Baek <kbbaek18@gmail.com>
2024-03-14 15:09:26 +0000

    include new routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
 """Main file for the template application."""
 from fastapi import FastAPI
 
-from app.items import router
+from app.api.items import router as items_router
+from app.api.user import router as user_router
 
 app = FastAPI(title="Stock Level API")
 
-app.include_router(router)
+app.include_router(items_router)
+app.include_router(user_router)
 
 
 @app.get("/")
